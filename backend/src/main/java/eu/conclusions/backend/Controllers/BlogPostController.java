815f581cce97cf66820d9953a8f07fee9f9b3568
rafal-ftw <rafal.w.draws@gmail.com>
2023-05-10 19:42:51 +0200

    dev init

--- backend/src/main/java/eu/conclusions/backend/Controllers/BlogPostController.java
+++
@@ -1,7 +0,0 @@
-package eu.conclusions.backend.Controllers;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class BlogPostController {
-}
--- backend/src/main/java/eu/conclusions/backend/Repositories/BlogPostRepository.java
+++
@@ -1,8 +0,0 @@
-package eu.conclusions.backend.Repositories;
-
-import eu.conclusions.backend.Entities.BlogPost;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface BlogPostRepository extends JpaRepository<BlogPost, Long> {
-}

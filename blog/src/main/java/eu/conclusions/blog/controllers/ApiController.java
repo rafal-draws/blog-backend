b9f31d32390970e2fd7e4c2d13b792d2116ea4e1
rafal-ftw <rafal.w.draws@gmail.com>
2023-05-23 12:45:04 +0200

    fixes

--- blog/src/main/java/eu/conclusions/blog/controllers/ApiController.java
+++ blog/src/main/java/eu/conclusions/blog/controllers/ApiController.java
@@ -2,18 +2,14 @@ package eu.conclusions.blog.controllers;
 
 import eu.conclusions.blog.models.BlogPost;
 import eu.conclusions.blog.services.BlogPostService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.Instant;
 
 @Controller
 public class ApiController {
 
     private BlogPostService blogPostService;
-    private static final Logger logger = LoggerFactory.getLogger(BlogPostController.class);
 
     public ApiController(BlogPostService theBlogPostService){
         blogPostService = theBlogPostService;
@@ -21,10 +17,7 @@ public class ApiController {
 
     @PostMapping("/api/posts")
     @ResponseBody
-    public BlogPost addPost(@RequestBody BlogPost blogPost){
-        blogPost.setId(0L);
-        blogPost.setSlug(blogPost.getTitle().toLowerCase().replace(" ", "-"));
-        blogPost.setDate(Instant.now());
+    public BlogPost addPost(@RequestBody BlogPost blogPost) {
         BlogPost dbBlogPost = blogPostService.save(blogPost);
         return dbBlogPost;
     }
